f9f0a0b93fea73b7649920bcc6b1b4a065d87982
Martin Spier <spiermar@gmail.com>
2014-02-09 18:59:47 -0800

    Including user agent information.

--- routes/beacon.js
+++ routes/beacon.js
@@ -11,8 +11,18 @@ var config = require('../config/config');
 var nano = require('nano')(config.db.url);
 var db = nano.use(config.db.name);
 
+
+
 exports.get = function(req, res){
-	db.insert({ 'timestamp': new Date().getTime(), 'view': req.params.view, 'ip': req.connection.remoteAddress, 'boomerang': req.query }, function(err, body, header) {
+	var ua = req.headers['user-agent'];
+	var r = require('ua-parser').parse(ua);
+	db.insert({ 'timestamp': new Date().getTime(), 
+				'view': req.params.view, 
+				'ip': req.connection.remoteAddress, 
+				'ua': { 'family': r.ua.family, 'major': r.ua.major, 'minor': r.ua.minor, 'patch': r.ua.patch },
+				'os': { 'family': r.os.family, 'major': r.os.major, 'minor': r.os.minor, 'patch': r.os.patch },
+				'device': { 'family': r.device.family },
+				'boomerang': req.query }, function(err, body, header) {
 		if (err) {
 			console.log('[db.insert] ', err.message);
 			return;
